a6bbc1b7801864107bfc4ccfc53c25137324d65f
Melissa Lopez <1zuzsuit492@Melissas-MacBook-Pro.local>
2021-10-14 17:37:05 -0400

    i tried.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const url = 'https://ghibliapi.herokuapp.com/films'
+const url = 'https://ghibliapi.herokuapp.com/films';
 //array of films//
 const dropdown = document.querySelector('#movie-titles');
 //selecting movie-titles
@@ -7,19 +7,27 @@ fetch(url).then((response) => response.json()).then((movies) => {
 //Populating dropdown menu with movie titles
         movies.forEach((movie) => {
         const option = document.createElement("option");
-        option.value = movie.title
+        option.value = movie.title;
         option.textContent = movie.title;
         dropdown.append(option);
-    })
-});
+        
+        dropdown.addEventListener("change", () => {
+            // event.preventDefault();
+            for (let movie of movies) {
+                if(dropdown.value === movie.title) {
+                    info.innerHTML = `<h3>${movie.title}</h3><p>${movie.release_date}</p><p>${movie.description}</p>` 
+                }
+                
+            }
+        })
+        });
+})
 document.querySelector("form").addEventListener("submit", (event) => {
     event.preventDefault(); //keeps page from refreshing
     const li = document.createElement("li") //creating an li 
     li.innerHTML = `<strong><b>${dropdown.value}</b></strong>: ${event.target.review.value}` 
     document.querySelector("ul").append(li) //inserting li in ul//
-    event.target.reset();
+    event.target.reset(); //resets textbox
 })
 
 
- 
-.catch(console.log)
